e38671064dbb520fa3c56cbb227cd2b7c714c931
muniza012 <munizaiqbal012@gmail.com>
2026-02-09 03:54:43 +0500

    chap 5 start

---
+++ script1-20.js
@@ -0,0 +1,224 @@
+////////////////////// ------Chap 1 "ALERTS"-------
+
+//////////  Ques # 1
+
+// alert('Welcome to this website')
+
+//////////  Ques # 2
+
+// alert('Error!Please enter a valid password')
+
+//////////  Ques # 3
+
+// alert('Welcome to JS Land...\nHappy Coding!')
+
+//////////  Ques # 4
+
+// alert('Welcome to JS Land')
+// alert('Happy Coding!\nPrevent this page from creating additional dialogs')
+
+//////////  Ques # 5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+
+
+
+
+
+////////////////////// ------Chap 2 "VARIABLES FOR STRINGS"-------
+
+//////////  Ques # 1
+
+// let username;
+
+//////////  Ques # 2
+
+// let myName='Muniza Iqbal';
+
+//////////  Ques # 3
+
+// let message='Hello World';
+// alert(message);
+
+//////////  Ques # 4
+
+// let name='John Doe';
+// let age= 15;
+// let education='Certified Mobile Application Development';
+
+// alert(`${name}
+// ${age} years old
+// ${education}`);
+
+//////////  Ques # 5
+
+// let word=`pizza\npizz\npiz\npi\np`
+// alert(word);
+
+//////////  Ques # 6
+
+// let email='abc@gmail.com';
+// alert('My email address is'+' ' + email);
+
+//////////  Ques # 7
+
+// let book= 'A smarter way to learn Javascript'
+// alert('I am trying to learn from the Book' + " " + book);
+
+//////////  Ques # 8
+
+// document.writeln(`<h2>Yeah I can write HTML content through Javascript</h2>`)
+
+//////////  Ques # 9
+ 
+// alert('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬')
+
+
+
+
+
+////////////////////// ------Chap 3 "VARIABLES FOR NUMBERS"-------
+
+//////////  Ques # 1
+
+// let age=20;
+// alert('I am'  + " " + age + " "  + 'years old.' );
+
+//////////  Ques # 2
+
+// let visited=14;
+// alert('You have visited this page' + " "+ visited + " " + 'times');
+
+
+//////////  Ques # 3
+
+// let birthYear=2005;
+// document.writeln(`<p>My birth year is ${birthYear}</p>
+// <p>Data type of my declared variable is number </p>`)
+
+//////////  Ques # 4
+
+// let name='John Doe';
+// let product='T-shirts (s)';
+// let quantity=3;
+//   document.writeln(`<p><b>${name}</b> has ordered <b>${quantity} ${product}</b> from XYZ Clothing store”.</p>`)
+
+
+
+
+////////////////////// ------Chap 4 "VARIABLE NAMES: LEGAL & ILLEGAL"-------
+
+//////////  Ques # 1
+
+// let name, age, email;
+
+//////////  Ques # 2
+
+//legal names: 
+// let user_info ,age ,$salary , user123 ,name;
+
+//illegal names: 
+// let 123name,user-profile,my email,@practice;
+
+//////////  Ques # 3
+
+console.log(`A heading stating “Rules for naming JS variables” `);
+console.log(`Variable names can only contain _ , characters , $ and numbers in the middle and end.`);
+console.log(`Variables must begin with a $name, _name or name.`);
+console.log(`Variable names are case sensitive`);
+console.log(`Variable names should not be JS keywords`);
+
+
+
+
+
+////////////////////// ------Chap 5 "MATH EXPRESSIONS"-------
+
+
+//////////  Ques # 1
+// let a=3;
+// let b=5;
+// let result=a+b;
+// console.log(`sum of ${a} and ${b} is ${result}`);
+
+
+
+
+//////////  Ques # 2
+// let a=3;
+// let b=5;
+// let result=a-b;
+// console.log(`subtraction of ${a} and ${b} is ${result}`);
+
+// let a=3;
+// let b=5;
+// let result=a*b;
+// console.log(`multiplication of ${a} and ${b} is ${result}`);
+
+// let a=3;
+// let b=5;
+// let result=a/b;
+// console.log(`division of ${a} and ${b} is ${result}`);
+
+
+// let a=3;
+// let b=5;
+// let result=a%b;
+// console.log(`modulus of ${a} and ${b} is ${result}`);
+
+//////////  Ques # 3
+let a;
+console.log(`Value after variable declaration is: ${a}`);
+a=5;
+console.log(`Initial value: ${a}`);
+a +=1;
+console.log(`Value after increment is: ${a}`);
+a +=7
+console.log(`Value after addition is: ${a}`);
+a -=1;
+console.log(`Value after decrement is: ${a}`);
+a %=3;
+console.log(`The remainder is ${a} `);
+
+
+//////////  Ques # 4
+
+
+//////////  Ques # 5
+
+
+//////////  Ques # 6
+
+
+//////////  Ques # 7
+
+
+//////////  Ques # 8
+
+
+
+//////////  Ques # 9
+
+
+//////////  Ques # 10
+
+
+//////////  Ques # 11
+
+
+//////////  Ques # 12
+
+
+//////////  Ques # 13
+
+
+
+
+
+
+
+
+
+////////////////////// ------Chap 6 "VARIABLE NAMES: LEGAL & ILLEGAL"-------
+
